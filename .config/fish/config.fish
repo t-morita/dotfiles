26ca650df01930197b4874ad065387aec9b711f7
Takeshi Morita <t_morita@keio.jp>
2018-12-13 10:12:53 +0900

    update fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,6 @@
 set EDITOR /usr/bin/vim
+set CONSCRIPT_HOME "$HOME/.conscript"
+set CONSCRIPT_OPTS "-XX:MaxPermSize=512M -Dfile.encoding=UTF-8"
 set PATH /usr/local/texlive/2017/bin/x86_64-darwin ~/anaconda/bin ~/bin ~/.conscript/bin $PATH 
 set PYTHONENCODING utf-8
-set PYTHONPATH /lib/pynaoqi-python2.7-2.1.4.13-linux64 $PYTHONPATH
 alias git=hub
