add6489088b076ee6f801d723e1086560cf6cd60
Takeshi Morita <takesi.morita@gmail.com>
2018-12-27 16:49:47 +0900

    update config.fish
    - fix PATH
    - added JAVA_HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,7 @@
 set EDITOR /usr/bin/vim
 set CONSCRIPT_HOME "$HOME/.conscript"
 set CONSCRIPT_OPTS "-XX:MaxPermSize=512M -Dfile.encoding=UTF-8"
-set PATH /usr/local/texlive/2017/bin/x86_64-darwin ~/anaconda/bin ~/bin ~/.conscript/bin $PATH 
+set PATH /usr/local/texlive/2018/bin/x86_64-darwin /anaconda3/bin ~/bin ~/.conscript/bin $PATH 
 set PYTHONENCODING utf-8
+set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/
 alias git=hub
