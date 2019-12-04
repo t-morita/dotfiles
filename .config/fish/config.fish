afaffc6824354caf7c5440a1c0d2784edad153af
Takeshi Morita <takesi.morita@gmail.com>
2019-12-04 22:55:44 +0900

    update config.fish
    - added fish_variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,13 @@
 set EDITOR /usr/bin/vim
 set CONSCRIPT_HOME "$HOME/.conscript"
 set CONSCRIPT_OPTS "-XX:MaxPermSize=512M -Dfile.encoding=UTF-8"
-set PATH /usr/local/texlive/2018/bin/x86_64-darwin /anaconda3/bin ~/bin ~/.conscript/bin $PATH 
 set PYTHONENCODING utf-8
-set JAVA_HOME /Library/Java/JavaVirtualMachines/jdk1.8.0_191.jdk/Contents/Home/
+set JAVA8_HOME /Library/Java/JavaVirtualMachines/java8_latest_home/
+set JAVA11_HOME /Library/Java/JavaVirtualMachines/java11_latest_home/
+set JAVA_HOME $JAVA11_HOME
+set PATH ~/.conscript/bin $JAVA_HOME $PATH 
 alias git=hub
+if status is-interactive
+and not set -q TMUX
+    exec tmux
+end
